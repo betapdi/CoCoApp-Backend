fc9a020e5053da587ea8ea04637887a7ef5701fc
BeTapDi <lecongquochan@gmail.com>
2024-08-28 01:56:22 +0700

    Initialize SpringBoot and MongoDB

---
+++ src/main/java/com/server/cocoapp/CoCoApplication.java
@@ -0,0 +1,13 @@
+package com.server.cocoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCoApplication.class, args);
+	}
+
+}
